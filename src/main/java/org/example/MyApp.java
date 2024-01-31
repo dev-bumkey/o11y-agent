9121da427bd577f1ff3d99293339b011f30b8170
bumkey <jsbebe02@acornsoft.io>
2024-01-31 17:12:38 +0900

    33

--- src/main/java/org/example/MyApp.java
+++ src/main/java/org/example/MyApp.java
@@ -1,35 +1,16 @@
 package org.example;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
 public class MyApp {
-    private final Logger log = LoggerFactory.getLogger(getClass());
     public static void main(String[] args) {
-        SpringApplication.run(MyApp.class, args);
-    }
-
-    @GetMapping(value = "/")
-    public String doGetHelloWorld() {
-        return "Hello World";
-    }
-
-    @GetMapping(value = "/demo")
-    public String doGetHelloWorldDemo() {
-        return "Hello World (Demo)";
-    }
-
-    @GetMapping(value = "/log")
-    public void doGetHelloWorldLog() {
-        log.info("hello world");
-        log.debug("debug world");
-        log.warn("warn world");
-        log.error("error world");
+        SpringApplication app = new SpringApplication(MyApp.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 }
