4d2aeabdef31f9ce8e11667821bbddb769f1b5fd
bumkey <jsbebe02@acornsoft.io>
2024-03-19 10:08:19 +0900

    30초 마다 랜덤 정수 로그 표현

--- src/main/java/org/example/MyApp.java
+++ src/main/java/org/example/MyApp.java
@@ -1,16 +1,37 @@
 package org.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
 @SpringBootApplication
 public class MyApp {
+
+    private static final Logger logger = LoggerFactory.getLogger("FILE");
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MyApp.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            double r = Math.random();
+            int intValue = (int) (r * 100) + 1;
+            logger.info("Random Value: {}", intValue);
+        }, 0, 30, TimeUnit.SECONDS); // 30초마다 실행
+
+        // 어플리케이션이 종료될 때 스케줄러도 종료되도록 합니다.
+        Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdown));
     }
 }
